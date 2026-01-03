7fd5173a7d7bc7df8c093dbf2e4c3310e5298425
Jean-Michel Conti <114805482+JeanConti@users.noreply.github.com>
2026-01-03 17:49:26 +0100

    feat: Add initial frontend pages and backend Prisma ORM setup.

--- 02 backend/prisma/prisma.service.ts
+++ 02 backend/prisma/prisma.service.ts
@@ -2,9 +2,13 @@ import { Injectable, OnModuleInit} from "@nestjs/common";
 import { PrismaClient} from "@prisma/client";
 
 @Injectable() 
-export class PrismaService extends PrismaClient
-implements OnModuleInit {
-  $connect: any;
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      errorFormat: 'minimal',
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
